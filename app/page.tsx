bd2e1cbc7f29b194c3d7fbdda3365ec188a7b13a
jilv220 <hkfhfkcoc@protonmail.com>
2023-05-07 12:34:34 +0800

    chore:fix RSC comment

--- app/page.tsx
+++ app/page.tsx
@@ -84,6 +84,7 @@ export default async function IndexPage({ searchParams }) {
             <main className='basis-3/4 items-center'>
               {articles.map((article: IArticle) => (
                 <div key={article.slug}>
+                  {/* @ts-expect-error Server Component */}
                   <ArticlePreview article={article}></ArticlePreview>
                 </div>
               ))}
